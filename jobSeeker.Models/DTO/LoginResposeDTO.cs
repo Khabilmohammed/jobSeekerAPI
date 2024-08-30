cf80105c22a8a79e3d48b3015e366d5e686ccbf6
Mohammed Khabil <khabilmohammed3@gmail.com>
2024-08-30 15:58:32 +0530

    connction_string_settedUp

---
+++ jobSeeker.Models/DTO/LoginResposeDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jobSeeker.Models.DTO
+{
+    public class LoginResposeDTO
+    {
+        public string Email { get; set; }
+        public string Token {  get; set; }  
+    }
+}
--- jobSeeker/Program.cs
+++ jobSeeker/Program.cs
@@ -1,10 +1,16 @@
+using jobSeeker.DataAccess.Data;
 using jobSeeker.DataAccess.Services.IWhetherForCastService;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultDBConnection")));
 builder.Services.AddControllers();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
